85cd15c10171c439796b41da8e889dc86ddcb56d
Rock Paper Linux <rockpaperlinux@gmail.com>
2013-11-12 10:35:35 +0100

    Script begin

---
+++ xubuntusetup.sh
@@ -0,0 +1,58 @@
+#Update
+sudo apt-get update
+sudo apt-get upgrade
+
+#Evernote
+sudo add-apt-repository ppa:vincent-c/nevernote
+
+#Libreoffice
+sudo add-apt-repository ppa:libreoffice/libreoffice-4-1
+
+#Conky manager
+sudo apt-add-repository -y ppa:teejee2008/ppa
+
+#Update
+sudo apt-get update
+sudo apt-get dist-upgrade
+
+#Libreoffice
+sudo apt-get install libreoffice
+
+#PHP
+sudo apt-get install php5-cli
+sudo apt-get install php5-json
+sudo apt-get install php5-curl
+#See http://www.howtogeek.com/howto/linux/show-the-list-of-installed-packages-on-ubuntu-or-debian/ for your packages
+
+#SSH
+sudo apt-get install openssh-server
+
+#Java
+sudo apt-get install default-jre
+
+#Package managers
+sudo apt-get install curl
+sudo apt-get install git
+
+#Evernote
+sudo apt-get install nixnote
+
+#Conky
+sudo apt-get install conky-manager
+
+#Removing useless stuff
+sudo apt-get remove gnome-sudoku
+sudo apt-get remove gnome-mines
+sudo apt-get remove gmusicbrowser
+sudo apt-get remove parole
+sudo apt-get remove xfburn
+sudo apt-get remove abiword
+sudo apt-get remove abiword-common abiword
+sudo apt-get remove libabiword-3.0 
+sudo apt-get remove gnumeric
+audo apt-get remove gnumeric common
+sudo apt-get remove gnumeric-doc
+
+#Update
+sudo apt-get update
+sudo apt-get upgrade
