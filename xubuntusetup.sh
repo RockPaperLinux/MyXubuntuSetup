322856e44235f193f170ae1ada836055882766b8
Rock Paper Linux <rockpaperlinux@gmail.com>
2013-11-12 10:46:57 +0100

    Added automation

--- xubuntusetup.sh
+++ xubuntusetup.sh
@@ -1,58 +1,58 @@
 #Update
-sudo apt-get update
+sudo apt-get update 
 sudo apt-get upgrade
 
 #Evernote
-sudo add-apt-repository ppa:vincent-c/nevernote
+sudo add-apt-repository -y ppa:vincent-c/nevernote
 
 #Libreoffice
-sudo add-apt-repository ppa:libreoffice/libreoffice-4-1
+sudo add-apt-repository -y ppa:libreoffice/libreoffice-4-1
 
 #Conky manager
 sudo apt-add-repository -y ppa:teejee2008/ppa
 
 #Update
-sudo apt-get update
-sudo apt-get dist-upgrade
+sudo apt-get -y update
+sudo apt-get -y dist-upgrade
 
 #Libreoffice
-sudo apt-get install libreoffice
+sudo apt-get -y install libreoffice
 
 #PHP
-sudo apt-get install php5-cli
-sudo apt-get install php5-json
-sudo apt-get install php5-curl
+sudo apt-get -y install php5-cli
+sudo apt-get -y install php5-json
+sudo apt-get -y install php5-curl
 #See http://www.howtogeek.com/howto/linux/show-the-list-of-installed-packages-on-ubuntu-or-debian/ for your packages
 
 #SSH
-sudo apt-get install openssh-server
+sudo apt-get -y install openssh-server &&
 
 #Java
-sudo apt-get install default-jre
+sudo apt-get -y install default-jre &&
 
 #Package managers
-sudo apt-get install curl
-sudo apt-get install git
+sudo apt-get -y install curl &&
+sudo apt-get -y install git &&
 
 #Evernote
-sudo apt-get install nixnote
+sudo apt-get -y install nixnote &&
 
 #Conky
-sudo apt-get install conky-manager
+sudo apt-get -y install conky-manager &&
 
 #Removing useless stuff
-sudo apt-get remove gnome-sudoku
-sudo apt-get remove gnome-mines
-sudo apt-get remove gmusicbrowser
-sudo apt-get remove parole
-sudo apt-get remove xfburn
-sudo apt-get remove abiword
-sudo apt-get remove abiword-common abiword
-sudo apt-get remove libabiword-3.0 
-sudo apt-get remove gnumeric
-audo apt-get remove gnumeric common
-sudo apt-get remove gnumeric-doc
+sudo apt-get -y remove gnome-sudoku &&
+sudo apt-get -y remove gnome-mines &&
+sudo apt-get -y remove gmusicbrowser &&
+sudo apt-get -y remove parole &&
+sudo apt-get -y remove xfburn &&
+sudo apt-get -y remove abiword &&
+sudo apt-get -y remove abiword-common abiword &&
+sudo apt-get -y remove libabiword-3.0  &&
+sudo apt-get -y remove gnumeric &&
+audo apt-get -y remove gnumeric common &&
+sudo apt-get -y remove gnumeric-doc &&
 
 #Update
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update &&
+sudo apt-get -y upgrade &&
